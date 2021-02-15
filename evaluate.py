feee32a0b2117c34b1f7e967cf7ac35e8ebd33cc
mrjleo <mrjleo@users.noreply.github.com>
2021-02-15 13:51:59 +0100

    compute MAP and MRR in evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,33 +1,39 @@
 #! /usr/bin/env python3
 
 
-import csv
 import argparse
-from collections import defaultdict
+from pathlib import Path
 
 import torch
+import numpy as np
+
+from qa_utils.util import read_output_files, write_trec_eval_file
+from qa_utils.metrics import average_precision, reciprocal_rank
 
 
 def main():
     ap = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     ap.add_argument('OUTPUT_FILES', nargs='+', help='Output files')
     ap.add_argument('--out_file', default='out.tsv', help='Output file to use with TREC-eval')
+    ap.add_argument('--rr_k', type=int, default=10, help='Compute MRR@k')
     args = ap.parse_args()
 
-    results = defaultdict(dict)
-    for f in args.OUTPUT_FILES:
-        print(f'reading {f}...')
-        for d in torch.load(f):
-            results[d['q_id']][d['doc_id']] = d['prediction'][0]
+    predictions, labels = read_output_files(map(Path, args.OUTPUT_FILES))
+    write_trec_eval_file(Path(args.out_file), predictions, 'bert')
 
-    print(f'writing {args.out_file}...')
-    with open(args.out_file, 'w', encoding='utf-8', newline='\n') as fp:
-        writer = csv.writer(fp, delimiter='\t')
-        for q_id in results:
-            ranking = sorted(results[q_id].keys(), key=results[q_id].get, reverse=True)
-            for rank, doc_id in enumerate(ranking, 1):
-                score = results[q_id][doc_id]
-                writer.writerow([q_id, 'Q0', doc_id, rank, score, 'bert'])
+    # compute MAP and MRR@k, in case the dataset is incompatible to trec-eval
+    aps, rrs = [], []
+    for q_id in predictions:
+        q_id_predictions, q_id_labels = [], []
+        for doc_id in predictions[q_id]:
+            q_id_predictions.append(predictions[q_id][doc_id])
+            q_id_labels.append(labels[q_id][doc_id])
+        pred_tensor = torch.FloatTensor(q_id_predictions)
+        label_tensor = torch.IntTensor(q_id_labels)
+        aps.append(average_precision(pred_tensor, label_tensor))
+        rrs.append(reciprocal_rank(pred_tensor, label_tensor, args.rr_k))
+    print(f'MAP: {np.mean(aps)}')
+    print(f'MRR@{args.rr_k}: {np.mean(rrs)}')
 
 
 if __name__ == '__main__':
